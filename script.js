0b088766576af69101967e6f212b087049d57202
summeradhal <summeradhal@gmail.com>
2016-08-03 16:03:18 -0400

    Commit 4

--- script.js
+++ script.js
@@ -24,13 +24,15 @@ context=canvas.getContext('2d');
 		tempMax=weatherData.list[0].main.temp_max;
 		tempMin=weatherData.list[0].main.temp_min;
 		weatherDescription=weatherData.list[0].weather[0].description;
+		weatherIcon=weatherData.list[0].weather[0].icon;
 		console.log(tempMax);
 		console.log(tempMin);
 		console.log(weatherDescription);
 		console.log(currentTemp);
 		animate(0,currentSkies);
-		drawBox(tempMax,tempMin,weatherDescription,currentTemp);
-		
+		drawBox(tempMax,tempMin,weatherDescription,currentTemp,weatherIcon);
+		var src1 = "http://openweathermap.org/img/w/"+weatherIcon+".png";
+                $('.weather-icon').attr("src", src1);
 		
 		console.log(context);
 		
@@ -82,41 +84,46 @@ context.fill();
 		
 };
 
+//info rectangle
 function drawBox(tempMax,tempMin,weatherDescription,currentTemp){
 
+
+//draw rectangle filled with info
 context.beginPath();
 context.rect(800,100,450,500);
 context.stroke();
 context.fillStyle="#ffffff";
 context.fill();
 
+//setting the color based on temp
+	if(currentTemp<70){
+		var tempColor='#2DC2B0';
+		//outer circle 
+	}else if((currentTemp>=70)&&(currentTemp<90)){
+		var tempColor='#EEA513';
+	}else if(currentTemp>=90){
+		var tempColor='#ff0';
+	}
 
-if(currentTemp<70){
-	var tempColor='#2DC2B0';
-	//outer circle 
-}else if((currentTemp>=70)&&(currentTemp<90)){
-	var tempColor='#EEA513';
-}else if(currentTemp>=90){
-	var tempColor='#ff0';
-}
-
-context.strokeStyle=tempColor;
-context.lineWidth=10;
-
-
-
+	//setting border around box to the color of the set temp color
+	context.strokeStyle=tempColor;
+	context.lineWidth=10;
 
 
 
-		context.fillStyle="#000"
-		context.font="28px Arial";
 
-		context.fillText("High: "+tempMin,820,470);
-		context.fillText('Low: '+tempMin,820,520);
-		context.font="20px Arial";
-		context.fillText(weatherDescription,970,200);
 
-	context.stroke();
+			//28 px font
+			context.fillStyle="#000"
+			context.font="28px Arial";
+			context.fillText("High: "+tempMin,820,470);
+			context.fillText('Low: '+tempMin,820,520);
+
+			//20 px font
+			context.font="20px Arial";
+			
+			context.fillText(weatherDescription,970,300);
+			context.stroke();
 
 
 };
